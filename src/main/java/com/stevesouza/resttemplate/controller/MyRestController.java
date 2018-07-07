9ca3736f68bb857304ef0d6b84f98998e0e2b7b7
Steve Souza <steve@stevesouza.com>
2018-07-07 14:21:18 -0400

    first commit of RestTemplate work

--- src/main/java/com/stevesouza/resttemplate/controller/MyRestController.java
+++ src/main/java/com/stevesouza/resttemplate/controller/MyRestController.java
@@ -1,24 +1,50 @@
 package com.stevesouza.resttemplate.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.ui.Model;
+import com.stevesouza.resttemplate.db.Post;
+import javafx.geometry.Pos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * Note when you have a @RestController you don't need to specify the return type as @ResponseBody as that is implied.
+ */
 
 @RestController
-@RequestMapping(value="/restapi")
+@RequestMapping(value="/resttemplate")
 public class MyRestController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
-    // get a person by Id
-    @GetMapping("/searchgov/{job}")
-    public Map<String, String> getPerson(@PathVariable("job") String job) {
-    	 Map<String, String> map=new HashMap<>();
-    	 map.put("fn", "steve");
-    	 map.put("ln", "souza");
-    	 return map;
+    @GetMapping("/getposts")
+    // @PathVariable("job")
+    public String getPosts() {
+    	 RestTemplate rest = new RestTemplate();
+    	 ResponseEntity<String> response = rest.getForEntity("https://jsonplaceholder.typicode.com/posts", String.class);
+    	 log.info("http status code: "+response.getStatusCode());
+    	 
+    	 return response.getBody();
+    }
+
+    @GetMapping("/getpostsasobjects")
+    public List<?> getPostsAsObjects() {
+        RestTemplate rest = new RestTemplate();
+        List<?> posts = rest.getForObject("https://jsonplaceholder.typicode.com/posts", List.class);
+        return posts;
+    }
+
+    @GetMapping("/getpostsdelme")
+    public Post[] getPostsAsObjectArray() {
+        RestTemplate rest = new RestTemplate();
+        // Note I added an extra field 'name' to the Post object and populated it by default and it will show
+        // up in the results.
+        Post[] posts = rest.getForObject("https://jsonplaceholder.typicode.com/posts", Post[].class);
+        return posts;
     }
 }
---
+++ src/main/java/com/stevesouza/resttemplate/db/Post.java
@@ -0,0 +1,66 @@
+package com.stevesouza.resttemplate.db;
+
+/**
+ *  populated with data from public api: https://jsonplaceholder.typicode.com/posts
+ *
+ *  Here is the json format it returns, but many are returned in array form.  Note just to be sure that I am creating
+ *  json and returning it to the front end as List<Post> I am adding an extra field populated with default data.
+ *  {
+ * userId: 1,
+ * id: 1,
+ * title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+ * body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
+ * },
+ *
+ *
+ */
+
+public class Post {
+    private long id;
+    private String userId;
+    private String title;
+    private String body;
+    private String name="Steve Souza";
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
