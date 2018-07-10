1c40ab983d68ada81a63a181dfd9896d69129a05
Steve Souza <steve@stevesouza.com>
2018-07-10 14:47:27 -0400

    added comments

--- src/main/java/com/stevesouza/resttemplate/MyConfiguration.java
+++ src/main/java/com/stevesouza/resttemplate/MyConfiguration.java
@@ -4,8 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Note @SpringBootApplication annotation also has @Configuration as part of its definition so this @Bean definition could
+ * just as easily been put in there.  Note this is the equivalent of defining spring beans in xml.
+ */
 @Configuration
 public class MyConfiguration {
+    /** factory method for spring bean creation of RestTemplate
+     *
+     * @return RestTemplate
+     */
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
--- src/main/java/com/stevesouza/resttemplate/ResttemplateApplication.java
+++ src/main/java/com/stevesouza/resttemplate/ResttemplateApplication.java
@@ -3,6 +3,10 @@ package com.stevesouza.resttemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * note @SpringBootApplication annotation also defines @Coniguration so you could define spring beans in this class by
+ * having a method and annotating it with @Bean.
+ */
 @SpringBootApplication
 public class ResttemplateApplication {
 
