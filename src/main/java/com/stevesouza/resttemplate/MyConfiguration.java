757bb33884152477e4096e334116f23ff4766429
Steve Souza <steve@stevesouza.com>
2018-07-12 15:42:40 -0400

    Added apache http client to maven to support the HTTP PATCH command.

--- src/main/java/com/stevesouza/resttemplate/MyConfiguration.java
+++ src/main/java/com/stevesouza/resttemplate/MyConfiguration.java
@@ -1,7 +1,12 @@
 package com.stevesouza.resttemplate;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -15,8 +20,25 @@ public class MyConfiguration {
      *
      * @return RestTemplate
      */
+    // https://howtodoinjava.com/spring-restful/resttemplate-httpclient-java-config/
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
+        return restTemplate;
     }
+
+//    @Autowired
+//    private CloseableHttpClient httpClient;
+
+
+
+    // don't think this needs to be a bean.  note i use httpcomments component as the default used
+    // by string is the jdk classes of HttpClient, but it doesn't support patch.
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        //clientHttpRequestFactory.setHttpClient(HttpClientBuilder.create().build());
+        return clientHttpRequestFactory;
+    }
+
 }
