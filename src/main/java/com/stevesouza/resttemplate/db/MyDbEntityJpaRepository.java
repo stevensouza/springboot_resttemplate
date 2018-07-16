8575ea2a584353e7bb21dcbdaef5b95ed152609a
Steve Souza <steve@stevesouza.com>
2018-07-16 11:21:50 -0400

    Fixed incorrect return type

--- src/main/java/com/stevesouza/resttemplate/db/MyDbEntityJpaRepository.java
+++ src/main/java/com/stevesouza/resttemplate/db/MyDbEntityJpaRepository.java
@@ -1,6 +1,5 @@
 package com.stevesouza.resttemplate.db;
 
-import com.stevesouza.resttemplate.controller.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -24,6 +23,6 @@ public interface MyDbEntityJpaRepository extends JpaRepository<MyDbEntity, Long>
      * @param name
      * @return List of Post objects
      */
-    List<Post> findByFirstName(@Param("name") String name);
+    List<MyDbEntity> findByFirstName(@Param("name") String name);
 
 }
