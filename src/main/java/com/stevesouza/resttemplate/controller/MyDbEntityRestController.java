8660135454f26a4e71b18c4b21bd5b527d9b0de5
Steve Souza <steve@stevesouza.com>
2018-07-16 16:26:18 -0400

    did other POST methods to help understand how the returned json works.
    Hal links are returned if ResponseEntity<String> is returned and plane json is returned if ResponseEntity<MyDbEntity> is returned.

--- src/main/java/com/stevesouza/resttemplate/controller/MyDbEntityRestController.java
+++ src/main/java/com/stevesouza/resttemplate/controller/MyDbEntityRestController.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Note when you have a @RestController you don't need to specify the return type as @ResponseBody as that is implied.
@@ -20,6 +23,12 @@ import org.springframework.web.client.RestTemplate;
  *
  * Note: I don't use a JpaRepository in this class, but a typical use case is to Autowire it into the constructor and call its methods
  * from the RestController.
+ *
+ *  HttpServletRequest, HttpServlet- Response, String, or numeric parameters that
+ *  correspond to query parameters in the request, model, cookie values, HTTP request header values, or a number of other possibilities.
+ *  p 172 spring in action.
+ *
+ * public String serveRest(@RequestBody String body, @RequestHeader HttpHeaders headers) {
  *
  * @author stevesouza
  */
@@ -43,19 +52,41 @@ public class MyDbEntityRestController {
         this.rest = rest;
     }
 
+
+    // Content-Type should be application/json and passed on from httpheaders.  methods post1, post2, post3
+    // all create a mydbentity though slightly different approaches.
+    // The following is probably preferred as it lets you pass in headers to the request as well as
+    // return json+hal format (i.e. a string)
+    @PostMapping("/mydbentity")
+    public  ResponseEntity<String> post1(@RequestBody MyDbEntity entity, @RequestHeader MultiValueMap<String, String> headers) {
+        // note if there were multiple arguments you still go ("first{} second{}", arg1, arg2) as it is order based.
+        log.info("submitted httpheaders: {}", headers);
+
+        // doing this as i need to pass on the headers
+        HttpEntity<MyDbEntity> requestEntity = new HttpEntity<>(entity, headers);
+        ResponseEntity<String> responseEntity = rest.exchange(BASE_URL, HttpMethod.POST, requestEntity, String.class);
+
+        log.info("POST ResponseEntity: {}", responseEntity);
+        return responseEntity;
+    }
+
     /**
      *
      * @param entity entity object to add.
      * @return ResponseEntity including added object
      */
-    @PostMapping("/mydbentity")
+    @PostMapping("/mydbentity/post2")
     // not idempotent.
     // http status code 201 Set the Location header to contain a link to the newly-created
     // resource (on POST). Response body content may or may not be present.
     // @ResponseStatus(HttpStatus.CREATED)
     // in this case i simply return the status code from the underlying api
     // note I return Post object, but String could just as easily be returned for raw json.
-    public  ResponseEntity<MyDbEntity> post(@RequestBody MyDbEntity entity) {
+    // note this post doesn't pass on headers and so regular json is returned.
+
+    // note this returns a MyDbEntity in the ResonseEntity. The result is that they hal links are
+    // stripped out and regular json is returned.
+    public  ResponseEntity<MyDbEntity> post2(@RequestBody MyDbEntity entity) {
         // note if there were multiple arguments you still go ("first{} second{}", arg1, arg2) as it is order based.
         log.info("submitted object to create: {}", entity);
         ResponseEntity<MyDbEntity> responseEntity = rest.postForEntity(BASE_URL, entity, MyDbEntity.class);
@@ -65,6 +96,30 @@ public class MyDbEntityRestController {
         return responseEntity;
         //return createdObject;
     }
+
+    @PostMapping("/mydbentity/post3")
+    // not idempotent.
+    // http status code 201 Set the Location header to contain a link to the newly-created
+    // resource (on POST). Response body content may or may not be present.
+    // @ResponseStatus(HttpStatus.CREATED)
+    // in this case i simply return the status code from the underlying api
+    // note I return Post object, but String could just as easily be returned for raw json.
+    // note this post doesn't pass on headers and so regular json is returned.
+
+    // same approach as post2 except a ResponseEntity<String> is returned instead of
+    // ResponseEntity<MyDbEntity>.  The result is that a string of json including data and
+    // hal links is returned instead of just plain json.
+    public  ResponseEntity<String> post3(@RequestBody MyDbEntity entity) {
+        // note if there were multiple arguments you still go ("first{} second{}", arg1, arg2) as it is order based.
+        log.info("submitted object to create: {}", entity);
+        ResponseEntity<String> responseEntity = rest.postForEntity(BASE_URL, entity, String.class);
+        // MyDbEntity createdObject = rest.postForObject(BASE_URL, entity, MyDbEntity.class);
+        // MyDbEntity createdObject = responseEntity.getBody();
+        log.info("POST ResponseEntity: {}", responseEntity);
+        return responseEntity;
+        //return createdObject;
+    }
+
 
     @GetMapping("/mydbentity")
     public String getAll() {
@@ -110,6 +165,7 @@ public class MyDbEntityRestController {
         log.info("PATCH responseEntity={}", responseEntity);
         return responseEntity;
     }
+
 
 
 }
