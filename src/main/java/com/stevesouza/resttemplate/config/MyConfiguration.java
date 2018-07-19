020b59ef5dc3862be0b480a41f7d524cf60df819
Steve Souza <steve@stevesouza.com>
2018-07-19 09:30:59 -0400

    created config package

--- src/main/java/com/stevesouza/resttemplate/config/MyConfiguration.java
+++ src/main/java/com/stevesouza/resttemplate/config/MyConfiguration.java
@@ -1,4 +1,4 @@
-package com.stevesouza.resttemplate;
+package com.stevesouza.resttemplate.config;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
