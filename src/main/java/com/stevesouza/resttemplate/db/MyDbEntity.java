8bbb6af8d2e0806cd21041339d944bc2665bb0c8
Steve Souza <steve@stevesouza.com>
2018-08-04 13:25:29 -0400

    used lombok instead of coding direclty

--- src/main/java/com/stevesouza/resttemplate/db/MyDbEntity.java
+++ src/main/java/com/stevesouza/resttemplate/db/MyDbEntity.java
@@ -1,5 +1,7 @@
 package com.stevesouza.resttemplate.db;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,6 +18,7 @@ import javax.persistence.Id;
  *  * 	@Max(100)
  */
 
+@Data
 @Entity
 public class MyDbEntity {
     @Id
@@ -26,40 +29,5 @@ public class MyDbEntity {
     private String firstName="Joe";
     private String lastName;
     private int age;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "MyDbEntity{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
 
 }
