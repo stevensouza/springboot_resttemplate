2635a8abb2a2bd489dc97068209076960ff632de
Steve Souza <steve@stevesouza.com>
2018-08-27 21:44:27 -0400

    changed order of methods

--- src/main/java/com/stevesouza/resttemplate/service/ServiceInt.java
+++ src/main/java/com/stevesouza/resttemplate/service/ServiceInt.java
@@ -8,13 +8,9 @@ import java.util.List;
 @Transactional
 public interface ServiceInt<V extends VOBase> {
     List<V> getAll();
-
+    V get(long id);
     V create(V vo);
-
+    V update(long id, V vo);
     // idempotent. returns 200 and content or 204 and no content
     void delete(long id);
-
-    V get(long id);
-
-    V update(long id, V vo);
 }
